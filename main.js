f49002d762010b2e88a97e68efbd72f9ac657618
魏如松 <weirusong@meituan.com>
2018-10-12 10:59:05 +0800

    add

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow, ipcMain, Notification} = require('electron')
+const notifier = require('node-notifier')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -27,7 +28,29 @@ function createWindow () {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', () => {
+  createWindow()
+
+  // use Notification API
+  function nativeNotifier () {
+    const nt = new Notification({
+      title: 'Notification API',
+      body: `${Date.now()}`,
+      sound: 'Basso.aiff'
+    })
+    nt.show()
+  }
+
+  // use node-notifier
+  const nodeNotifier = () => {
+    notifier.notify({
+      title: 'nodeNotifier',
+      message: `${Date.now()}`
+    })
+  }
+  ipcMain.on('native-notification', nativeNotifier)
+  ipcMain.on('node-notifier-notification', nodeNotifier)
+})
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
